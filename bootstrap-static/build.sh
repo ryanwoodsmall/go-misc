760afce50f160a578ea724cd3f001ecd36dc1ab1
ryanwoodsmall <rwoodsmall@gmail.com>
2019-09-30 02:33:09 +0000

    update to 1.13.1

--- bootstrap-static/build.sh
+++ bootstrap-static/build.sh
@@ -43,10 +43,10 @@ gobsdir="go${gobsver}"
 gobsfile="go${gobsver}.tar.gz"
 gobsfilesha256="f4ff5b5eb3a3cae1c993723f3eab519c5bae18866b5e5f96fe1102f0cb5c3e52"
 # go intermediate and final build verison
-gover="1.13"
+gover="1.13.1"
 godir="go${gover}"
 gofile="go${gover}.src.tar.gz"
-gofilesha256="3fc0b8b6101d42efd7da1da3029c0a13f22079c0c37ef9730209d8ec665bf122"
+gofilesha256="81f154e69544b9fa92b1475ff5f11e64270260d46e7e36c34aafc8bc96209358"
 # download
 gobaseurl="https://dl.google.com/go"
 gobsurl="${gobaseurl}/${gobsfile}"
