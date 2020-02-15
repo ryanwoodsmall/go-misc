85d467d5f235a57654bcb713791a2a12ffd1d118
ryanwoodsmall <rwoodsmall@gmail.com>
2020-02-15 07:36:19 +0000

    update to go 1.13.8

--- bootstrap-static/build.sh
+++ bootstrap-static/build.sh
@@ -43,10 +43,10 @@ gobsdir="go${gobsver}"
 gobsfile="go${gobsver}.tar.gz"
 gobsfilesha256="f4ff5b5eb3a3cae1c993723f3eab519c5bae18866b5e5f96fe1102f0cb5c3e52"
 # go intermediate and final build verison
-gover="1.13.7"
+gover="1.13.8"
 godir="go${gover}"
 gofile="go${gover}.src.tar.gz"
-gofilesha256="e4ad42cc5f5c19521fbbbde3680995f2546110b5c6aa2b48c3754ff7af9b41f4"
+gofilesha256="b13bf04633d4d8cf53226ebeaace8d4d2fd07ae6fa676d0844a688339debec34"
 # download
 gobaseurl="https://dl.google.com/go"
 gobsurl="${gobaseurl}/${gobsfile}"
