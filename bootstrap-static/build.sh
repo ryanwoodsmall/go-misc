c5aaa84c4a0159d86c178fd86609b5156538c38d
ryanwoodsmall <rwoodsmall@gmail.com>
2023-12-07 19:08:27 -0600

    update golang to 1.21.5

--- bootstrap-static/build.sh
+++ bootstrap-static/build.sh
@@ -63,11 +63,11 @@ gobs1filesha256="ccf36b53fb0024a017353c3ddb22c1f00bc7a8073c6aac79042da24ee34434d
 gofilesha256sums["${gobs1file}"]="${gobs1filesha256}"
 
 # go intermediate and final build verison
-: ${gover:="1.21.4"}
+: ${gover:="1.21.5"}
 gomajver="${gover%%.*}"
 gominver="${gover#*.}"
 gominver="${gominver%%.*}"
-: ${gofilesha256:="47b26a83d2b65a3c1c1bcace273b69bee49a7a7b5168a7604ded3d26a37bd787"}
+: ${gofilesha256:="285cbbdf4b6e6e62ed58f370f3f6d8c30825d6e56c5853c66d3c23bcdb09db19"}
 godir="go${gover}"
 gofile="go${gover}.src.tar.gz"
 gofilesha256sums["${gofile}"]="${gofilesha256}"
