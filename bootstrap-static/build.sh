f66bc9c7204ffc6ec40bc44294d10f88fc1e11f0
ryanwoodsmall <rwoodsmall@gmail.com>
2025-09-22 19:50:15 -0500

    make boostrap chain easier to extend

--- bootstrap-static/build.sh
+++ bootstrap-static/build.sh
@@ -11,88 +11,75 @@
 # XXX - include _${TIMESTAMP} in directory/archive?
 # XXX - look at using src/bootstrap.bash instead of custom
 # XXX - include curl cert.pem in ${cwsw}/go/current/etc/ssl/cert.pem as fallback?
-# XXX - go 1.20+ bootstrap requires go 1.17.3+ - see: https://go.dev/doc/go1.20#bootstrap
-# XXX - convert to using arrays/hashes with actual functions - arbitrary # of steps, 0->1->...->n
-# XXX - this downloads _every_ time, need to... not do that?
 # XXX - at some point it'll be go 1.4->1.17.3+->1.2x.x->1.3x.x(?)->2.x.x, would be good to be prepared
-# XXX - getting unweildy, need to functionalize bootstrap builds
 #
 
 set -eu
 
 sname="${BASH_SOURCE[0]}"
 
-if [[ ! $(uname -m) =~ x86_64 ]] ; then
-  echo "${sname}: please run on amd64 (for now)" 1>&2
+function failexit() {
+  while [[ ${#} -gt 0 ]] ; do
+    printf '%s: %s\n' "${sname}" "${1}" 1>&2
+    shift
+  done
   exit 1
+}
+
+if [[ ! $(uname -m) =~ x86_64 ]] ; then
+  failexit "please run on amd64 (for now)"
 fi
 
 # prerequisite programs
 prereqs=( 'curl' 'gcc' 'gzip' 'nproc' 'tar' 'xz' )
 for prereq in ${prereqs[@]} ; do
-  if ! $(hash "${prereq}" >/dev/null 2>&1) ; then
-    echo "${sname}: ${prereq} not found" 1>&2
-    exit 1
+  if ! command -v "${prereq}" &>/dev/null ; then
+    failexit "${prereq} not found"
   fi
 done
 
 # curl options
-copts="-k -L -O"
+: ${copts:="-k -L -o"}
 
 # check for real xz, not busybox
 if ! `xz --version 2>&1 | grep -qi 'xz utils'` ; then
-  echo "${sname}: please install xz" 1>&2
-  exit 2
+  failexit "please install xz"
 fi
 
-# checksums
-declare -A gofilesha256sums
+# versions
+declare -a govers
+govers+=( "1.4-bootstrap-20171003" )
+govers+=( "1.19.13" )
+govers+=( "1.21.13" )
+govers+=( "1.23.12" )
+govers+=( "1.25.1" )
+: ${gofinalver:="${govers[-1]}"}
 
-# bootstrap go stage 0 version in C
-gobs0ver="1.4-bootstrap-20171003"
-gobs0dir="go${gobs0ver}"
-gobs0file="go${gobs0ver}.tar.gz"
-gobs0filesha256="f4ff5b5eb3a3cae1c993723f3eab519c5bae18866b5e5f96fe1102f0cb5c3e52"
-gofilesha256sums["${gobs0file}"]="${gobs0filesha256}"
+# files, urls
+: ${godlbase:="https://go.dev/dl"}
+declare -A godirh gofileh gofileurlh gofilesha256h
+for v in ${govers[@]} ; do
+  godirh["${v}"]="go${v}"
+  gofileh["${v}"]="${godirh[${v}]}.src.tar.gz"
+  gofileurlh["${v}"]="${godlbase}/${gofileh[${v}]}"
+done
+gofilesha256h["1.4-bootstrap-20171003"]="f4ff5b5eb3a3cae1c993723f3eab519c5bae18866b5e5f96fe1102f0cb5c3e52"
+gofilesha256h["1.19.13"]="ccf36b53fb0024a017353c3ddb22c1f00bc7a8073c6aac79042da24ee34434d3"
+gofilesha256h["1.21.13"]="71fb31606a1de48d129d591e8717a63e0c5565ffba09a24ea9f899a13214c34d"
+gofilesha256h["1.23.12"]="e1cce9379a24e895714a412c7ddd157d2614d9edbe83a84449b6e1840b4f1226"
+gofilesha256h["1.25.1"]="d010c109cee94d80efe681eab46bdea491ac906bf46583c32e9f0dbb0bd1a594"
+# 1.4 special handling
+gofileh['1.4-bootstrap-20171003']="${godirh['1.4-bootstrap-20171003']}.tar.gz"
+gofileurlh['1.4-bootstrap-20171003']="${godlbase}/${gofileh['1.4-bootstrap-20171003']}"
 
-# go stage 1 bootstrap 1.17+ for go 1.20+
-gobs1ver="1.19.13"
-gobs1dir="go${gobs1ver}"
-gobs1file="go${gobs1ver}.src.tar.gz"
-gobs1filesha256="ccf36b53fb0024a017353c3ddb22c1f00bc7a8073c6aac79042da24ee34434d3"
-gofilesha256sums["${gobs1file}"]="${gobs1filesha256}"
-
-# go stage 2 bootstrap 1.20+ for go 1.22+
-gobs2ver="1.21.13"
-gobs2dir="go${gobs2ver}"
-gobs2file="go${gobs2ver}.src.tar.gz"
-gobs2filesha256="71fb31606a1de48d129d591e8717a63e0c5565ffba09a24ea9f899a13214c34d"
-gofilesha256sums["${gobs2file}"]="${gobs2filesha256}"
-
-# go stage 3 bootstrap 1.22+ for go 1.24+
-gobs3ver="1.23.12"
-gobs3dir="go${gobs3ver}"
-gobs3file="go${gobs3ver}.src.tar.gz"
-gobs3filesha256="e1cce9379a24e895714a412c7ddd157d2614d9edbe83a84449b6e1840b4f1226"
-gofilesha256sums["${gobs3file}"]="${gobs3filesha256}"
-
-# go intermediate and final build verison
-: ${gover:="1.25.1"}
+: ${gover:="${gofinalver}"}
 gomajver="${gover%%.*}"
 gominver="${gover#*.}"
 gominver="${gominver%%.*}"
-: ${gofilesha256:="d010c109cee94d80efe681eab46bdea491ac906bf46583c32e9f0dbb0bd1a594"}
-godir="go${gover}"
-gofile="go${gover}.src.tar.gz"
-gofilesha256sums["${gofile}"]="${gofilesha256}"
-
-# download
-gobaseurl="https://dl.google.com/go"
-gobs0url="${gobaseurl}/${gobs0file}"
-gobs1url="${gobaseurl}/${gobs1file}"
-gobs2url="${gobaseurl}/${gobs2file}"
-gobs3url="${gobaseurl}/${gobs3file}"
-gourl="${gobaseurl}/${gofile}"
+: ${godir:="go${gover}"}
+: ${gofile:="go${gover}.src.tar.gz"}
+: ${gofileurl:="${godlbase}/${gofile}"}
+: ${gofilesha256:="${gofilesha256h[${gover}]}"}
 
 # architectures
 goarches=( '386' 'amd64' 'arm' 'arm64' 'riscv64' )
@@ -103,13 +90,15 @@ cwtop="/usr/local/crosware"
 cwbuild="${cwtop}/builds"
 cwdl="${cwtop}/downloads"
 cwtmp="${cwtop}/tmp"
+cwgotmp="${cwtmp}/go"
 cwsw="${cwtop}/software"
 : ${rtdir:="${cwsw}/go${gomajver}${gominver}"}
 
 # random
-godldir="${cwdl}/go"
-vartmp="/var/tmp"
+: ${godldir:="${cwdl}/go"}
+: ${vartmp:="/var/tmp"}
 
+# surround a string in a box
 # XXX - tb="++++$(echo ${@} | sed 's/./,/g')"
 function boxecho() {
   local len="$(($(echo -n ${@} | wc -c)+4))"
@@ -121,109 +110,96 @@ function boxecho() {
   echo
 }
 
+# fetch/check function
+function fetchcheck() {
+  if [[ ${#} != 3 ]] ; then
+    failexit 'provide: ${url} ${downloadfile} ${checksum}'
+  fi
+  mkdir -p "$(dirname -- ${2})"
+  if [[ -e "${2}" ]] ; then
+    if sha256sum "${2}" | awk '{print $1}' | grep -qi "^${3}$" ; then
+      printf '%s: %s exists and matched checksum\n' "${sname}" "${2}"
+      return
+    fi
+  fi
+  printf '%s: %s\n' "${sname}" "downloading ${1} to ${2}"
+  curl ${copts} "${2}" "${1}"
+  printf '%s: %s\n' "${sname}" "checking SHA-256 sum of ${2}"
+  sha256sum "${2}" | awk '{print $1}' | grep -qi "^${3}$" || failexit "could not download/checksum ${1} to ${2}"
+}
+
+# bootstrap build
+bspv=""
+function gobsbuild() {
+  if [[ ${#} -ne 1 ]] ; then
+    failexit 'provide one known bootstrap version'
+  fi
+  local v="${1}"
+  local gbe=''
+  pushd "${cwbuild}" &>/dev/null
+  local d="${godirh[${v}]}"
+  test -e go && rm -rf go
+  test -e "${d}" && rm -rf "${d}"
+  tar -zxf "${godldir}/${gofileh[${v}]}"
+  mv go "${d}"
+  pushd "${d}/src/" &>/dev/null
+  boxecho "building go ${v} in ${PWD}"
+  test -z "${bspv}" || export gbe="GOROOT_BOOTSTRAP=${cwbuild}/${godirh[${bspv}]}"
+  env GO_LDFLAGS='-extldflags "-static -s" -s -w' CGO_ENABLED=0 ${gbe} bash make.bash
+  popd &>/dev/null
+  popd &>/dev/null
+  rm -rf go
+  bspv="${v}"
+  echo
+}
+
 # create build/install directories
 mkdir -p "${cwbuild}"
 mkdir -p "${cwtmp}"
+mkdir -p "${cwgotmp}"
 mkdir -p "${rtdir}"
 mkdir -p "${godldir}"
 mkdir -p "${vartmp}"
 
-# download
-pushd "${godldir}"
-for url in "${gobs0url}" "${gobs1url}" "${gobs2url}" "${gobs3url}" "${gourl}" ; do
-  boxecho "fetching ${url} in ${PWD}"
-  curl ${copts} "${url}"
+# downloads
+boxecho "+++++ DOWNLOAD +++++"
+for v in ${govers[@]} ; do
+  fetchcheck "${gofileurlh[${v}]}" "${godldir}/${gofileh[${v}]}" "${gofilesha256h[${v}]}"
 done
+fetchcheck "${gofileurl}" "${godldir}/${gofile}" "${gofilesha256}"
+echo
 
-# checksum
-for k in ${!gofilesha256sums[@]} ; do
-  sha256sum "${k}" | awk '{print $1}' | grep -q "^${gofilesha256sums[${k}]}$" || {
-    echo "${k} SHA-256 sum mismatch" 1>&2
-    exit 1
-  }
+# bootstraps
+boxecho "+++++ BOOTSTRAP +++++"
+pushd "${cwbuild}" &>/dev/null
+for ((i = 0 ; i < ${#govers[@]} ; i++)) ; do
+  boxecho "+++ STAGE ${i} +++"
+  gobsbuild "${govers[${i}]}"
+  fbsv="${bspv}"
 done
-popd
-
-# bootstrap
-pushd "${cwbuild}"
-
-# stage 0: 1.4 (in C)
-test -e go && rm -rf go
-test -e "${gobs0dir}" && rm -rf "${gobs0dir}"
-tar -zxf "${godldir}/${gobs0file}"
-mv go "${gobs0dir}"
-pushd "${gobs0dir}/src/"
-boxecho "building stage0 go${gobs0ver} in ${PWD}"
-env GO_LDFLAGS='-extldflags "-static -s" -s -w' CGO_ENABLED=0 bash make.bash
-popd
-echo
-
-# stage 1: second stage bootstrap (go -> go 1.17+)
-test -e go && rm -rf go
-test -e "${gobs1dir}" && rm -rf "${gobs1dir}"
-tar -zxf "${godldir}/${gobs1file}"
-mv go "${gobs1dir}"
-pushd "${gobs1dir}/src/"
-boxecho "building stage1 go${gobs1ver} in ${PWD}"
-env GO_LDFLAGS='-extldflags "-static -s" -s -w' CGO_ENABLED=0 GOROOT_BOOTSTRAP="${cwbuild}/${gobs0dir}" bash make.bash
-popd
-echo
-
-# stage 2: third stage bootstrap (go 1.17+ -> go 1.20+)
-test -e go && rm -rf go
-test -e "${gobs2dir}" && rm -rf "${gobs2dir}"
-tar -zxf "${godldir}/${gobs2file}"
-mv go "${gobs2dir}"
-pushd "${gobs2dir}/src/"
-boxecho "building stage2 go${gobs2ver} in ${PWD}"
-env GO_LDFLAGS='-extldflags "-static -s" -s -w' CGO_ENABLED=0 GOROOT_BOOTSTRAP="${cwbuild}/${gobs1dir}" bash make.bash
-popd
-echo
-
-# stage 3: fourth stage bootstrap (go 1.20+ -> go 1.22+)
-test -e go && rm -rf go
-test -e "${gobs3dir}" && rm -rf "${gobs3dir}"
-tar -zxf "${godldir}/${gobs3file}"
-mv go "${gobs3dir}"
-pushd "${gobs3dir}/src/"
-boxecho "building stage3 go${gobs3ver} in ${PWD}"
-env GO_LDFLAGS='-extldflags "-static -s" -s -w' CGO_ENABLED=0 GOROOT_BOOTSTRAP="${cwbuild}/${gobs2dir}" bash make.bash
-popd
-echo
-
-# stage 4: fifth stage bootstrap (go 1.22+ -> go 1.24+)
-test -e go && rm -rf go
-test -e "${godir}" && rm -rf "${godir}"
-tar -zxf "${godldir}/${gofile}"
-mv go "${godir}"
-pushd "${godir}/src/"
-boxecho "building stage4 go${gover} in ${PWD}"
-env GO_LDFLAGS='-extldflags "-static -s" -s -w' CGO_ENABLED=0 GOROOT_BOOTSTRAP="${cwbuild}/${gobs3dir}" bash make.bash
-popd
-
-popd
+popd &>/dev/null
 echo
 
 # final builds
-pushd "${rtdir}"
+pushd "${rtdir}" &>/dev/null
 for goarch in ${goarches[@]} ; do
   boxecho "building final go${gover} for ${goarch} in ${PWD}"
   goarchdir="${godir}-${goarch}"
-  goarchive="${cwtmp}/${goarchdir}.tar"
+  goarchive="${cwgotmp}/${goarchdir}.tar"
   test -e go && rm -rf go
   test -e "${goarchdir}" && rm -rf "${goarchdir}"
   tar -zxf "${godldir}/${gofile}"
   mv go "${goarchdir}"
-  pushd "${goarchdir}/src/"
+  pushd "${goarchdir}/src/" &>/dev/null
   boxecho "patching crypto/x509/root_linux.go with cert locations"
   cat crypto/x509/root_linux.go > crypto/x509/root_linux.go.ORIG
   sed -i 's|"/etc/ssl/cert.pem"|"/etc/ssl/cert.pem","'"${cwtop}/etc/ssl/cert.pem"'"|g' crypto/x509/root_linux.go
   sed -i 's|"/etc/ssl/certs"|"/etc/ssl/certs","'"${cwtop}/etc/ssl/certs"'"|g' crypto/x509/root_linux.go
-  "${cwbuild}/${godir}/bin/gofmt" -w crypto/x509/root_linux.go
+  "${cwbuild}/${godirh[${fbsv}]}/bin/gofmt" -w crypto/x509/root_linux.go
   #diff -Naur crypto/x509/root_linux.go{.ORIG,} || true
-  env GO_LDFLAGS='-extldflags "-static -s" -s -w' CGO_ENABLED=0 GOROOT_BOOTSTRAP="${cwbuild}/${godir}" GOOS='linux' GOARCH="${goarch}" bash make.bash
-  popd
-  pushd "${goarchdir}"
+  env GO_LDFLAGS='-extldflags "-static -s" -s -w' CGO_ENABLED=0 GOROOT_BOOTSTRAP="${cwbuild}/${godirh[${fbsv}]}" GOOS='linux' GOARCH="${goarch}" bash make.bash
+  popd &>/dev/null
+  pushd "${goarchdir}" &>/dev/null
   rm -rf pkg/obj/go-build
   rm -rf pkg/bootstrap
   if [[ ! ${goarch} =~ amd64 ]] ; then
@@ -233,20 +209,21 @@ for goarch in ${goarches[@]} ; do
     mv bin/linux_${goarch}/* bin/
     rmdir bin/linux_${goarch}
   fi
-  popd
+  popd &>/dev/null
   boxecho "successfully built go${gover} for ${goarch}"
   echo "archiving ${goarchdir} to ${goarchive}"
   tar -cf "${goarchive}" "${goarchdir}/"
-  pushd "$(dirname ${goarchive})"
+  pushd "$(dirname ${goarchive})" &>/dev/null
   echo "using xz to compress ${goarchive} in ${PWD}"
   rm -f "${goarchive}.xz"
   xz --threads=$(nproc) -e -v -v "${goarchive}"
   echo "storing SHA-256 sum to ${goarchive}.xz.sha256"
   sha256sum "$(basename ${goarchive}.xz)" > "${goarchive}.xz.sha256"
-  popd
+  popd &>/dev/null
   echo
 done
-popd
+popd &>/dev/null
 echo
+
 
 # vim: set ft=bash:
